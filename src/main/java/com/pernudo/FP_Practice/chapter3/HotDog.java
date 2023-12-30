320b2b8bc940991bb6114968a3a3372046ad2f3a
Pernudo <tipodeincognito@gmail.com>
2023-12-30 22:32:14 +0100

    Chapter 3. Supplier

---
+++ src/main/java/com/pernudo/FP_Practice/chapter3/HotDog.java
@@ -0,0 +1,9 @@
+package com.pernudo.FP_Practice.chapter3;
+
+public final class HotDog {
+
+    @Override
+    public String toString() {
+        return "Soy el hot dog más rico del lugar";
+    }
+}
---
+++ src/main/java/com/pernudo/FP_Practice/chapter3/SupplierExample.java
@@ -0,0 +1,28 @@
+package com.pernudo.FP_Practice.chapter3;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+
+    public static void main (String[] args){
+        Supplier<LocalDate> localDateSupplier = LocalDate::now;
+        var random = new Random();
+        Supplier<Integer> integerSupplier = random::nextInt;
+        Supplier<HotDog> hotDogSupplier = HotDog::new;
+
+        usingSupplier(localDateSupplier);
+        usingSupplier(integerSupplier);
+        usingSupplier(integerSupplier);
+        usingSupplier(integerSupplier);
+        usingSupplier(integerSupplier);
+        usingSupplier(hotDogSupplier);
+    }
+
+    static <T> void usingSupplier(Supplier<T> supplier){
+        System.out.println("Hacemos cierto código");
+        System.out.println("Se obtiene: " + supplier.get());
+    }
+
+}
