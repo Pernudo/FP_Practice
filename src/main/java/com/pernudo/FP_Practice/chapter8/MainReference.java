135ab00718478671808358e40dddabccfa9124b1
Pernudo <tipodeincognito@gmail.com>
2024-01-04 16:00:23 +0100

    Chapter 8. Method Reference

---
+++ src/main/java/com/pernudo/FP_Practice/chapter8/MainReference.java
@@ -0,0 +1,48 @@
+package com.pernudo.FP_Practice.chapter8;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class MainReference {
+
+    public static void main (String[] args){
+
+        /*
+         * Formato --> referencia :: nombre del método
+         * Tipos -->{
+         *              nombreDeLaClase::métodoStatic
+         *              nombreDeLaClase::método
+         *              nombreDeLaClase::new
+         *              objetoExistente::método
+         *          }
+         */
+        var str = "Marcos";
+        var instance = new MainReference();
+        Supplier<String> instanceMethod1 = str::toUpperCase;
+        Supplier<String> instanceMethod2 =  instance::getAnything;
+        Supplier<String> instanceMethod3 =  instance::getReferencedMethod;
+        Supplier<MainReference> constructorReference =  MainReference::new;
+        Predicate<String> staticMethod = Objects::isNull;
+
+        System.out.println(instanceMethod1.get());
+        System.out.println(instanceMethod2.get());
+        System.out.println(instanceMethod3.get());
+        System.out.println(constructorReference.get().toString());
+        System.out.println(staticMethod.test(null));
+    }
+
+    public String getAnything(){
+        return "Cualquier cosa";
+    }
+
+    public String getReferencedMethod(){
+        Supplier<String> supplier = this::getAnything;
+        return supplier.get().concat(" --> Llamado con this");
+    }
+
+    @Override
+    public String toString() {
+        return "Soy una instancia de MainReference";
+    }
+}
